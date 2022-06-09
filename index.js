5ce450f428bcca2d393a4af94043fd73bcbe6b12
mavinsi <markynrodrisilva@gmail.com>
2022-06-08 22:13:08 -0300

    organizing structuring

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const { Client, Intents, DiscordAPIError } = require('discord.js');
-const fs = require('fs')
+// MyBotHelper 0.2
+// https://github.com/mavinsi/mybothelper
+
+
+
+// MBH Configs & dependencies
 version = '0.2'
 exports.version = version;
-const discord = require('discord.js')
 const botcfg = require('./config/bot.json')
+
+// Dependencies
+const fs = require('fs')
+const discord = require('discord.js')
+
+const { Client, Intents, DiscordAPIError } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
 console.log(`# Welcome to MyBotHelper ${version}`)
 console.log(`# github.com/mavinsi/mybothelper \n`)
 console.log(`[MBH] Initializing...`)
+
 client.on('ready', () => {
-   console.log(`[MBH] starting initial.js...`);
    client.commands.get('initial').execute(client)
     console.log(`[MBH] ${botcfg.name} online`);
 });
 client.commands = new discord.Collection();
+
 console.log(`[MBH] Loading commands...`);
 const commandFiles = fs.readdirSync('./cmds/').filter(file => file.endsWith('.js'))
 console.log(commandFiles)
 console.log(`[MBH] Commands loaded successfully`);
+
 for(const file of commandFiles){
   const command = require(`./cmds/${file}`)
    client.commands.set(command.name, command)
@@ -27,8 +40,7 @@ client.on('messageCreate', async message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-
-    
+  
     for(const file of commandFiles){
       const command = require(`./cmds/${file}`)
       if(cmd === command.name){
@@ -36,11 +48,6 @@ client.on('messageCreate', async message => {
       client.commands.get(command.name).execute(message,args)
     }
     }
-
-
-
-
-
 
 });
 client.login(botcfg.token)
