3f157d413044582e6f6a1dcabcc44ab675f55dc2
mavinsi <markynrodrisilva@gmail.com>
2022-06-08 21:39:46 -0300

    improving the 'say' command

--- cmds/say.js
+++ cmds/say.js
@@ -2,7 +2,6 @@ module.exports= {
     name: 'say',
     description: 'Testando',
     execute(message,args){
-        console.log(args.join().replaceAll(",",""))
-        message.channel.send(args.join().replaceAll(","," "))
+       message.channel.send(args.join(' '))
     }
 }
